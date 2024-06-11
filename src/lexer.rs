68c56962a34bc191108228e23fee61510ce0688c
Joshua Willis <joshua.willis@intellitect.com>
2024-06-10 19:44:11 -0700

    Add phf, lex first keyword

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,6 +1,11 @@
+use phf::phf_map;
 use std::{
     iter::{Iterator, Peekable},
     str::Chars,
+};
+
+static KEYWORDS: phf::Map<&'static str, LexToken> = phf_map! {
+    "let" => LexToken::Let,
 };
 
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -18,6 +23,9 @@ pub enum LexToken {
 
     // Delimiters
     Semicolon,
+
+    // Error
+    Error(String),
 }
 
 pub struct Lexer<'a> {
@@ -28,14 +36,55 @@ impl<'a> Lexer<'a> {
     pub fn new(input: &'a str) -> Self {
         Lexer { input: input.chars().peekable(), }
     }
+
+    fn read_to_delimiter(&mut self) -> String {
+        let mut buffer = String::new();
+
+        while let Some(c) = self.input.peek() {
+            if is_delimiter(*c) {
+                break;
+            }
+
+            buffer.push(self.input.next().unwrap());
+        }
+
+        buffer
+    }
+
+    fn read_identifier_or_keyword(&mut self) -> Result<LexToken, String> {
+        let identifier = self.read_to_delimiter();
+        if identifier.is_empty() {
+            return Err("Attempted to read, but found nothing.".into());
+        }
+
+        if let Some(token) = try_get_keyword(identifier.as_str()) {
+            Ok(token)
+        } else {
+            Ok(LexToken::Identifier(identifier))
+        }
+    }
 }
 
 impl<'a> Iterator for Lexer<'a> {
     type Item = LexToken;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        match self.input.peek() {
+            Some(c) if c.is_alphabetic() => match self.read_identifier_or_keyword() {
+                Ok(identifier) => Some(identifier),
+                Err(err) => Some(LexToken::Error(err))
+            }
+            _ => None,
+        }
     }
+}
+
+fn try_get_keyword(keyword: &str) -> Option<LexToken> {
+    Some(KEYWORDS.get(keyword)?.clone())
+}
+
+fn is_delimiter(c: char) -> bool {
+    c == ' ' || c == ';'
 }
 
 #[cfg(test)]
